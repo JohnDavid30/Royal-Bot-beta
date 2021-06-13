ff2affa302058f694a634930f3345e725bbae2d2
ADEF12 <71268369+Ontel03@users.noreply.github.com>
2021-06-13 11:40:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 //Bot Token - Important
-exports.Owner = "539005361332682752"; //Bot Owner ID - Important
-exports.Developer = "539005361332682752"; //Bot Dev ID - Important
-exports.Default_Prefix = "x!"; //Bot Default Prefix (Examples: A!, !)- Important
-exports.Color = "RANDOM"; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) - Important
-exports.Support = "https://saweria.co/AdeFahrozi"; //Support Server Link - Never Gonna Give You Up (If No Link Provided)
-exports.Dashboard = "https://electosite.glitch.me"; //Dashboard Your Bot
-exports.Server_ID = "809744236324192256"; // ID Your Serverexports.Server_ID = "826826587592065095"; // ID Your Server
+exports.Owner = ""; //Bot Owner ID
+exports.Developer = ""; //Bot Dev ID
+exports.Default_Prefix = ""; //Bot Default Prefix 
+exports.Color = ""; //Bot All Embeds Color - Use CAPS For Name (Examples: BLUE, RANDOM) 
+exports.Support = ""; //Support Server Link - 
+exports.Dashboard = ""; //Dashboard Your Bot
+exports.Server_ID = ""; // ID Your Server
